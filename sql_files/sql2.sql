ae9a4fe86e49828ed8468cab90129c1ccd292e8f
sairamdgr8 <sairamdgr8@gmail.com>
2021-11-21 22:28:47 +0530

    sqlite connection setup

---
+++ sql_files/sql2.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE sai2( 
+firstname text, 
+lastname text, 
+age integer
+);
+CREATE TABLE sai_book2( 
+title text, 
+author text 
+);
+INSERT INTO 
+sai_book2(title, author) 
+VALUES ( 
+'Dan Clarke''s GFG Detective Agency', 
+'Sean Simpsons' 
+);
+INSERT INTO 
+sai2(firstname,lastname,age)
+VALUES(
+'sai',
+'Doe',
+27
+);
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
