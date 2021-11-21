74dc453b4622c15660fa11e3764fedb779f36b6c
sai <36527057+sairamdgr8@users.noreply.github.com>
2021-11-21 18:41:09 +0530

    Create sql1.sql

---
+++ sql_files/sql1.sql
@@ -0,0 +1,35 @@
+CREATE TABLE sai( 
+firstname text, 
+lastname text, 
+age integer
+);
+CREATE TABLE sai_book( 
+title text, 
+author text 
+);
+INSERT INTO 
+sai_book(title, author) 
+VALUES ( 
+'Dan Clarke''s GFG Detective Agency', 
+'Sean Simpsons' 
+);
+INSERT INTO 
+sai(firstname,lastname,age)
+VALUES(
+'sai',
+'Doe',
+27
+);
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
