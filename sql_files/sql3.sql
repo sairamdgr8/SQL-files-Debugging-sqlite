ef58fd05a9990a28ca857067cba49ba190270c43
sairamdgr8 <sairamdgr8@gmail.com>
2021-11-22 08:37:38 +0530

    sqlite connection setup

---
+++ sql_files/sql3.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE sai3( 
+firstname text, 
+lastname text, 
+age integer
+);
+CREATE TABLE sai_book3( 
+title text, 
+author text 
+);
+INSERT INTO 
+sai_book3(title, author) 
+VALUES ( 
+'Dan Clarke''s GFG Detective Agency', 
+'Sean Simpsons' 
+);
+INSERT INTO 
+sai3(firstname,lastname,age)
+VALUES(
+'sai',
+'Doe',
+27
+);
+
